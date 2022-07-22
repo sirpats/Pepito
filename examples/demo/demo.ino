ddd6eafa90cc808ed5601b9fd003f6a7dd30f240
Sir Pats <65472905+sirpats@users.noreply.github.com>
2022-07-23 03:32:45 +0800

    Update demo.ino

--- examples/demo/demo.ino
+++ examples/demo/demo.ino
@@ -5,10 +5,11 @@ Pepito robot(9); // 9 volts (use 3.8 if using USB power)
 void setup() {
   // put your setup code here, to run once:
 
-  robot.move(FORWARD, 10, 2000);delay(500);
-  robot.move(REVERSE, 10, 2000);delay(500);
-  robot.move(LEFT, 10, 2000);delay(500);
-  robot.move(RIGHT, 10, 2000);delay(500);
+  // format: robot.move(direction, power/percentage, duration in milliseconds)
+  robot.move(FORWARD, 10, 500);
+  robot.move(REVERSE, 10, 500);
+  robot.move(LEFT, 10, 500);
+  robot.move(RIGHT, 10, 500);
 
   robot.blink(100);
   robot.beep(128,50);
@@ -20,7 +21,8 @@ void loop() {
   robot.screen.clear();
   robot.screen.set2X();
   robot.screen.print("DIST: ");
-  robot.screen.println(robot.distance(2,4));
+  //robot.screen.println(robot.distanceX(2,4)); // alternative function
+  robot.screen.println(robot.distance());  
   robot.screen.print("LTRACKER:");
   robot.screen.println(robot.leftTracker());
   robot.screen.print("RTRACKER:");
